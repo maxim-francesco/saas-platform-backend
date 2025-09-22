febbca54df26facdaf9fa83037947532dfee6b50
maxim-francesco <maaximfrancesco@gmail.com>
2025-09-23 01:23:00 +0300

    fixed cors problems

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes"); // Importă rutele
 const categoryRoutes = require("./routes/categoryRoutes");
 const listingRoutes = require("./routes/listingRoutes");
@@ -8,6 +9,13 @@ const publicRoutes = require("./routes/publicRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// 2. Configurează CORS pentru a permite cereri DOAR de la adresa frontend-ului tău
+const corsOptions = {
+  origin:
+    "https://6000-firebase-studio-1758560203835.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev",
+};
+app.use(cors(corsOptions));
 
 // Middleware pentru a putea parsa body-ul request-urilor JSON
 app.use(express.json());
