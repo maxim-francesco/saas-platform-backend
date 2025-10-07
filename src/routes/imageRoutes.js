b17e2d9f2406ea799c0255fe568a382bbb19b5ab
maxim-francesco <maaximfrancesco@gmail.com>
2025-10-07 19:41:34 +0300

    Forcing route fix 11111111111

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,28 +1,11 @@
-// src/routes/imageRoutes.js
 const express = require("express");
-const {
-  rotateImage,
-  updateImageOrder,
-  testRoute,
-} = require("../controllers/imageController");
+const { rotateImage } = require("../controllers/imageController");
 const { isAuthenticated } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.use((req, res, next) => {
-  console.log(
-    `[ROUTER DEBUG] A intrat o cerere în imageRoutes. METODA: ${req.method}, URL: ${req.originalUrl}`
-  );
-  next();
-});
-
 router.use(isAuthenticated);
 
-router.get("/test", testRoute);
-
-// --- MODIFICARE CHEIE: Ruta mai specifică este acum PRIMA ---
-router.put("/order/:listingId", updateImageOrder);
-
-// Ruta mai generală este acum a DOUA
+// Rută pentru a roti o imagine
 router.put("/:imageId/rotate", rotateImage);
 
 module.exports = router;
