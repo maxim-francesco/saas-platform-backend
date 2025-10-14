e764f1a3ea7243428c0363b836deabbe510c930c
maxim-francesco <maaximfrancesco@gmail.com>
2025-10-14 12:16:02 +0300

    hello test

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -4,6 +4,16 @@ const { rotateImage } = require("../controllers/imageController");
 const { isAuthenticated } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
+// --- Middleware "Spion" ---
+// Acest cod se va executa pentru FIECARE cerere care ajunge la /api/images
+router.use((req, res, next) => {
+  console.log(
+    `[SPY] A intrat o cerere în imageRoutes. METODA: ${req.method}, URL: ${req.originalUrl}`
+  );
+  next(); // Trimite cererea mai departe
+});
+// --- Sfârșit Middleware ---
+
 router.use(isAuthenticated);
 
 // Rută pentru a roti o imagine existentă
