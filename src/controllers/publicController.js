f8fd2f13db16402b410af4c6dfed86ccfa9bdae4
maxim-francesco <maaximfrancesco@gmail.com>
2025-11-07 21:46:04 +0200

    not sold v3

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -41,6 +41,8 @@ const searchListings = async (req, res) => {
 
     const whereConditions = [];
 
+    whereConditions.push({ status: "AVAILABLE" });
+
     if (businessId) {
       whereConditions.push({ businessId: businessId });
     }
